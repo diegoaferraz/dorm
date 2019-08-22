d0fb3504b4f94e44276cdbeeee51c430eeb9e9ad
Diego Ferraz <diegoaferraz@gmail.com>
2019-08-22 18:28:59 -0300

    simple orm

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-date_default_timezone_set('America/Sao_Paulo');
-define('DB_DRIVER', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'loja');
-define('DB_USER', 'root');
-define('DB_PASS', '');
